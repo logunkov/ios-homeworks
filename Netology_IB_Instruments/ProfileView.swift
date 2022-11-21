ad000b8a5c98cd51debc600e1d8cda60d40f8a2e
logunkov <logunkovkn@gmail.com>
2022-11-21 18:58:55 +0700

    Add ProfileView.swift

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Constantin on 21.11.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var Image: UIImageView!
+    @IBOutlet weak var City: UILabel!
+    @IBOutlet weak var DataOfBirtdhay: UILabel!
+    @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var Text: UITextView!
+}
